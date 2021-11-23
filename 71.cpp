e2704de599741547636dcaa3e9d7c4dda733b6fd
Cookie Yoon <80456800+cookie-yoon@users.noreply.github.com>
2021-11-23 11:24:54 +0900

    Update 71.cpp

--- 71.cpp
+++ 71.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
 
+int main(){
+  char pw[11];
+  cout << "Insert the password to quit the program." << endl;
+  while(true){
+    cout << "password >> ";
+    cin >> pw;
+    if(strcmp(pw, "0000") == 0){
+      cout << "Quit the program normally." << endl;
+      break;
+    }
+    else
+      cout << "Wrong password" << endl;
+  }
+}
