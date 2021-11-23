579b1966756441cbe4329265c2b138b76983a0ef
Cookie Yoon <80456800+cookie-yoon@users.noreply.github.com>
2021-11-23 11:08:21 +0900

    Create 63.cpp

---
+++ 63.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Insert s >> ";
+  
+  int s;
+  cin >> s;
+  
+  cout << "Insert h >> ";
+  
+  int h;
+  cin >> h;
+  
+  int A = s * h;
+  cout << "A = " << A << '\n';
+}
