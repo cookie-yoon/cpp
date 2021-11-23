3b5e392be16076090756d3b079652e0c19dd4da5
Cookie Yoon <80456800+cookie-yoon@users.noreply.github.com>
2021-11-23 11:10:37 +0900

    Create 70.cpp

---
+++ 70.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Insert the name >> ";
+  
+  char name[11];
+  cin >> name;
+  
+  cout << "The name is " << name << '\n';
+  
+}
