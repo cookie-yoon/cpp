45a06b7fd9d8c00216817d0109e55af40357d416
Cookie Yoon <80456800+cookie-yoon@users.noreply.github.com>
2021-11-26 11:38:05 +0900

    Create 83-4.cpp

---
+++ 83-4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cout;
+
+int main(){
+  int year = 1;
+  int n = 1;
+  
+  cout << n + 200 << '\n';
+  // In the program, 'endl' cannot be used as it belongs to std.
+  
+  year = 2021;
+  
+  cout << year;
+}
