afbcfaf6b4e5320ce15311fd1ba6cdc8fcd86d64
Cookie Yoon <80456800+cookie-yoon@users.noreply.github.com>
2021-11-26 11:45:21 +0900

    Create 83-5.cpp

---
+++ 83-5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "myheader.h"
+
+using namespace std;
+
+int main(){
+  cout << MAX << endl << MIN;
+  return 0;
+}
+
+/*
+  // myheader.h
+  #define MAX 100;
+  #define MIN 0;
+  // How can I separate header file from *.cpp?
+*/
