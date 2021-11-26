dafe7be9e0ba4c25efe115abe70aeb681e5b0a0e
Cookie Yoon <80456800+cookie-yoon@users.noreply.github.com>
2021-11-26 12:06:50 +0900

    Create 85.cpp

---
+++ 85.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int age = 22;
+  char dept[20] = "Computer Science";
+  // We no longer use form like this: char* dept = "Computer Science";
+  
+  cout << age << ' ' << dept;
+}
