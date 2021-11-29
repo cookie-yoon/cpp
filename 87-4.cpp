11e0baa455bd69f052d6e7670578e9ffbf930ed3
Cookie Yoon <80456800+cookie-yoon@users.noreply.github.com>
2021-11-29 16:00:16 +0900

    Update 87-4.cpp

--- 87-4.cpp
+++ 87-4.cpp
@@ -5,10 +5,14 @@ int main(){
   char text[100];
   cout << "Insert the letters. (under 100)" << endl;
   
-  cin.getline(text, '\n');
+  cin.getline(text, 100);
+  // 'cin.getline(objectArray, maxLength);' saves letters including space.
   
-  cout << "The number of letters is " << sizeof(text) << '.';
+  int count = 0;
+  for(int i = 0; i < 100 ; i++){
+    if(text[i] == 'x')
+      count++;
+  }
+   
+  cout << "The number of 'x' is " << count << '.';
 }
-
-// have to be edited
-// using cin.getline
