c1456c8db1351d977edf9a2f90584db5aff6dd30
Cookie Yoon <80456800+cookie-yoon@users.noreply.github.com>
2021-12-05 19:23:37 +0900

    Update 88-3.cpp

--- 88-3.cpp
+++ 88-3.cpp
@@ -9,8 +9,16 @@ int main(){
   getline(cin, input);
   
   istringstream names(input);
-  for(int i = 1;getline(names, output, ';');i++)
+  int len = 0;
+  string longest;
+  for(int i = 1;getline(names, output, ';');i++){
     cout << i << ". " << output << endl;
+    if(output.length() > len){
+      len = output.length();
+      longest = output;
+    }
+  }
+  cout << "The longest name is " << longest << '.';
 }
 
 // checked here: https://stackoverflow.com/questions/11719538/how-to-use-stringstream-to-separate-comma-separated-strings
