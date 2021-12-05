a743634a6b82a5de87ff4fc1cbd896be1bd48fb6
Cookie Yoon <80456800+cookie-yoon@users.noreply.github.com>
2021-12-05 18:52:49 +0900

    Update 88-2.cpp

--- 88-2.cpp
+++ 88-2.cpp
@@ -9,7 +9,7 @@ int main(){
 
   while(1){
     cout << "Insert 'yes' to quit. >> ";
-    cin.getline(input, 100);
+    cin.getline(input, 100); // Inserting text longer then length, you would find the infinite loop.
 
     if(strcmp(input, key) == 0) // strcmp(charArr1, charArr2) returns 0 in case they're same. In others, it does depending on ascii code.
       break;
